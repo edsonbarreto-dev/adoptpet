e1f4d4499896ed735480b213f7c5308b237e06b1
Edson Barreto <edson.barreto@outlook.com>
2024-01-20 10:55:04 -0300

    refactory

--- src/main/java/br/com/adoptpet/core/dataprovider/solicitacao/BuscarSolicitacaoPorNumero.java
+++ src/main/java/br/com/adoptpet/core/dataprovider/solicitacao/BuscarSolicitacaoPorNumero.java
@@ -1,5 +1,7 @@
 package br.com.adoptpet.core.dataprovider.solicitacao;
 
-public interface BuscarSolicitacaoPorNumero<T> {
-    T find(final Long id);
+import br.com.adoptpet.core.domain.solicitacao.Solicitacao;
+
+public interface BuscarSolicitacaoPorNumero {
+    Solicitacao find(final Long numero);
 }
--- src/main/java/br/com/adoptpet/core/usecase/solicitacao/impl/AlterarSolicitacaoUseCaseImpl.java
+++ src/main/java/br/com/adoptpet/core/usecase/solicitacao/impl/AlterarSolicitacaoUseCaseImpl.java
@@ -3,30 +3,16 @@ package br.com.adoptpet.core.usecase.solicitacao.impl;
 import br.com.adoptpet.core.dataprovider.solicitacao.AlterarSolicitacao;
 import br.com.adoptpet.core.domain.solicitacao.Solicitacao;
 import br.com.adoptpet.core.usecase.solicitacao.AlterarSolicitacaoUseCase;
-import br.com.adoptpet.core.usecase.solicitacao.BuscarSolicitacaoPorNumeroUseCase;
-import br.com.adoptpet.dataprovider.repository.entity.SolicitacaoEntity;
-import br.com.adoptpet.dataprovider.repository.mapper.SolicitacaoEntityMapper;
 
 public class AlterarSolicitacaoUseCaseImpl implements AlterarSolicitacaoUseCase {
-
-    private final BuscarSolicitacaoPorNumeroUseCase<SolicitacaoEntity> buscarSolicitacaoPorNumeroUseCase;
-
-    private final SolicitacaoEntityMapper solicitacaoEntityMapper;
-
     private final AlterarSolicitacao alterarSolicitacao;
-    public AlterarSolicitacaoUseCaseImpl(
-            BuscarSolicitacaoPorNumeroUseCase buscarSolicitacaoPorNumeroUseCase,
-            AlterarSolicitacao solicitacao,
-            SolicitacaoEntityMapper solicitacaoEntityMapper
-    ) {
-        this.buscarSolicitacaoPorNumeroUseCase = buscarSolicitacaoPorNumeroUseCase;
+
+    public AlterarSolicitacaoUseCaseImpl(AlterarSolicitacao solicitacao) {
         this.alterarSolicitacao = solicitacao;
     }
 
     @Override
     public void update(Solicitacao solicitacao) {
-        SolicitacaoEntity entity = buscarSolicitacaoPorNumeroUseCase.findByNumero(solicitacao.getNumeroSolicitacao());
-        SolicitacaoEntityMapper
-        alterarSolicitacao.update(entity);
+        alterarSolicitacao.update(solicitacao);
     }
 }
